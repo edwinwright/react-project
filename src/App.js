0999e1fba6d7c48a3eec17ccc3382e8f327b51ae
katsuraku <kirsten.jones@ft.com>
2016-08-07 15:42:09 +0100

    Reduxy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import Routes from './config/Routes';
+import reducer from './reducers/index';
+
+let store = createStore(reducer);
 
 const App = () => (
-  <Router history={ hashHistory }>
-    {Routes}
-  </Router>
+  <Provider store={store}>
+    <Router history={ hashHistory }>
+      {Routes}
+    </Router>
+  </Provider>
 );
 
 export default App
--- src/containers/PhotoListContainer.js
+++ src/containers/PhotoListContainer.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import PhotoList from '../components/PhotoList'
+import { connect } from 'react-redux';
+import PhotoList from '../components/PhotoList';
+import * as actions from '../actions/photos';
 
 // NOTE: Do I need to import this stuff ??
-import es6promise from 'es6-promise';
+// import es6promise from 'es6-promise';
 // import 'isomorphic-fetch';
 
 class PhotoListContainer extends Component {
 
   constructor () {
     super();
-    this.state = {
-      photos: []
-    };
   }
 
   componentDidMount() {
@@ -24,16 +23,28 @@ class PhotoListContainer extends Component {
     })
     .then(response => response.json()) // What is this response object that's coming back ??
     .then(data => {
-      this.setState({photos : data.slice(0, 50) });
+      console.log(data.slice(0, 50));
+      this.props.receivePhotos(data.slice(0, 50));
     })
     .catch(console.log);
   }
 
   render() {
+    // console.log('props', this.props)
     return (
-      <PhotoList {...this.props} photos={this.state.photos} />
+      <PhotoList {...this.props} photos={this.props.photos} />
     );
   }
 }
 
-export default PhotoListContainer
+// Get stuff out of state and put it into props in this container.
+function mapStateToProps(state) {
+  return { photos: state.photos }
+}
+
+// Put functions into props so you can call them
+function mapDispatchToProps(dispatch) {
+  return { receivePhotos: (photos) => { dispatch(actions.receivePhotos(photos)) } }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PhotoListContainer)
